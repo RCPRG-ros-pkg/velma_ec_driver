ad8cca20476115f5a93a5855cc263b236b2c5b8d
Dawid Seredyński <dawid.seredynski@gmail.com>
2017-08-07 11:09:15 +0200

    cpu set = true by default

--- src/velma_ec_driver.cpp
+++ src/velma_ec_driver.cpp
@@ -292,7 +292,7 @@ int main(int argc, char *argv[])
     param.sched_priority = 10;
     pthread_setschedparam(pthread_self(), SCHED_FIFO, &param);
 
-    unsigned int cpu_affinity = 3;
+    unsigned int cpu_affinity = 4;
     cpu_set_t cs;
     CPU_ZERO(&cs);
     for(unsigned i = 0; i < 8*sizeof(cpu_affinity); i++)
